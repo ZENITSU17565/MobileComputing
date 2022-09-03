d796369e22f17169a58995003ae09064a448441b
ZENITSU17565 <1032200256@tcetmimbai.in>
2022-09-03 22:36:15 +0530

    Login page/Activity

---
+++ Tasks/AndroidDemoApp/MobileComputingTasks/app/src/main/java/com/singhvikrant/mobilecomputingtasks/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.singhvikrant.mobilecomputingtasks
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val showPass: ImageView = findViewById(R.id.show_pass_btn)
+        val hidePass: ImageView = findViewById(R.id.hide_pass_btn)
+        val password:EditText = findViewById(R.id.userPass)
+        val skip:TextView = findViewById(R.id.skip)
+
+        showPass.setOnClickListener {
+            showPass.visibility = View.GONE
+            hidePass.visibility = View.VISIBLE
+            password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+
+        }
+        hidePass.setOnClickListener {
+            showPass.visibility = View.VISIBLE
+            hidePass.visibility = View.GONE
+            password.transformationMethod = PasswordTransformationMethod.getInstance()
+        }
+
+        skip.setOnClickListener {
+            val message = "New Activity"
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
+            finish()
+        }
+    }
+}
