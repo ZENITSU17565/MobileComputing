4603c45c38ae5d1541a69fbfe4ae222270e2eadc
ZENITSU17565 <1032200256@tcetmimbai.in>
2022-10-02 22:19:14 +0530

    update research paper

---
+++ Tasks/AndroidDemoApp/MobileComputingTasks/app/src/main/java/com/singhvikrant/mobilecomputingtasks/ui/AboutUsActivity.kt
@@ -0,0 +1,12 @@
+package com.singhvikrant.mobilecomputingtasks.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.singhvikrant.mobilecomputingtasks.R
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
---
+++ Tasks/AndroidDemoApp/MobileComputingTasks/app/src/main/java/com/singhvikrant/mobilecomputingtasks/ui/ContactUsActivity.kt
@@ -0,0 +1,12 @@
+package com.singhvikrant.mobilecomputingtasks.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.singhvikrant.mobilecomputingtasks.R
+
+class ContactUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+    }
+}
